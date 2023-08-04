79f95b52b83d366893ee50b2bab1d497991603d7
michael.zhang <michael.zhang@kcsit.pt>
2023-08-04 14:16:15 +0000

    switching form to ref

--- src/Presentation/Components/Form.tsx
+++ src/Presentation/Components/Form.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useContext, useRef, useState } from "react";
 import { Marmota } from "../../Infrastructure/Models/Marmota";
 import { MarmotaContext } from "../../Infrastructure/Context/MarmotaContext";
 import { MarmotaController } from "../../Infrastructure/Controllers/ServiceController/MarmotaController";
@@ -7,24 +7,19 @@ export const Form = () => {
   const marmotaController = new MarmotaController();
   const { marmota } = useContext(MarmotaContext);
   const [marmotaState, setMarmotaState] = marmota;
+  const nameInput = useRef<HTMLInputElement>(null);
+  const ageInput = useRef<HTMLInputElement>(null);
+  const heightInput = useRef<HTMLInputElement>(null);
+  const weightInput = useRef<HTMLInputElement>(null);
 
-  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    const { name, age, height, weight } =
-      event.target as typeof event.target & {
-        name: { value: string };
-        age: { value: number };
-        height: { value: number };
-        weight: { value: number };
-      };
-
+  async function handleSubmit() {
+    console.log(nameInput.current!.value)
     let data: Marmota = {
       id: "",
-      name: name.value,
-      age: age.value,
-      height: height.value,
-      weight: weight.value,
+      name: nameInput.current!.value,
+      age: parseFloat(ageInput.current!.value),
+      height: parseFloat(heightInput.current!.value),
+      weight: parseFloat(weightInput.current!.value),
       actions: false,
     };
 
@@ -35,24 +30,24 @@ export const Form = () => {
   }
 
   return (
-    <form onSubmit={(event) => handleSubmit(event)}>
+    <>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name"></input>
+        <input ref={nameInput} type="text" id="name" name="name"></input>
       </div>
       <div>
         <label htmlFor="age">Age</label>
-        <input type="text" id="age" name="age"></input>
+        <input ref={ageInput} type="text" id="age" name="age"></input>
       </div>
       <div>
         <label htmlFor="height">Height</label>
-        <input type="text" id="height" name="height"></input>
+        <input ref={heightInput} type="text" id="height" name="height"></input>
       </div>
       <div>
         <label htmlFor="weight">Weight</label>
-        <input type="text" id="weight" name="weight"></input>
+        <input ref={weightInput} type="text" id="weight" name="weight"></input>
       </div>
-      <button type="submit">Create</button>
-    </form>
+      <button onClick={() => handleSubmit()}>Create</button>
+    </>
   );
 };
